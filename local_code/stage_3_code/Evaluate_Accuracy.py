d23d18258045e23617a026e2b97d3a4aea714b7d
Ivan Savkin <ivsavkin@ucdavis.edu>
2026-04-29 21:10:33 -0700

    copied code into part 3

---
+++ local_code/stage_3_code/Evaluate_Accuracy.py
@@ -0,0 +1,62 @@
+'''
+Concrete Evaluate class for a specific evaluation metrics
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from local_code.base_class.evaluate import evaluate
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+class Evaluate_Accuracy(evaluate):
+    data = None
+    
+    def evaluate(self):
+        print('evaluating performance...')
+
+        y_true = self.data['true_y'] #labels from test set
+        y_pred = self.data['pred_y'] #labels predicted by the model
+
+        #used macro to average the metrics equally
+        acc = accuracy_score(y_true, y_pred)
+        precision_weighted = precision_score(y_true, y_pred, average='weighted')
+        precision_macro = precision_score(y_true, y_pred, average='macro')
+        precision_micro = precision_score(y_true, y_pred, average='micro')
+
+        recall_weighted = recall_score(y_true, y_pred, average='weighted')
+        recall_macro = recall_score(y_true, y_pred, average='macro')
+        recall_micro = recall_score(y_true, y_pred, average='micro')
+
+
+        f1_weighted = f1_score(y_true, y_pred, average='weighted')
+        f1_macro = f1_score(y_true, y_pred, average='macro')
+        f1_micro = f1_score(y_true, y_pred, average='micro')
+
+
+        #prints the metrics 
+        print("Accuracy:",acc)
+        print("Precision (weighted):",precision_weighted)
+        print("Precision (macro):",precision_macro)
+        print("Precision (micro):",precision_micro)
+        print("Recall (weighted):",recall_weighted)
+        print("Recall (macro):",recall_macro)
+        print("Recall (micro):",recall_micro)
+        print("F1 (weighted):", f1_weighted)
+        print("F1 (macro):", f1_macro)
+        print("F1 (micro):", f1_micro)
+
+
+        #returns all metrics
+        return {
+            'acc':                acc,
+            'f1_weighted':        f1_weighted,
+            'f1_macro':           f1_macro,
+            'f1_micro':           f1_micro,
+            'recall_weighted':    recall_weighted,
+            'recall_macro':       recall_macro,
+            'recall_micro':       recall_micro,
+            'precision_weighted': precision_weighted,
+            'precision_macro':    precision_macro,
+            'precision_micro':    precision_micro,
+        }
---
+++ local_code/stage_3_code/Method_MLP.py
@@ -0,0 +1,78 @@
+
+from local_code.base_class.method import method
+from local_code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+import torch
+from torch import nn
+import numpy as np
+
+
+class Method_MLP(method, nn.Module):
+    data = None
+    max_epoch = 20 #change if necessary
+    learning_rate = 1e-3 #change if necessary
+
+    def __init__(self, mName, mDescription):
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
+
+        # Input = 784 features
+        # Output = 10 classes
+
+        #Input (784) → Hidden (256) → Hidden (128) → Output (10)
+        self.fc_layer_1 = nn.Linear(784, 256) #model input must accept 784 numbers (28 x 28 images)
+        self.activation_func_1 = nn.ReLU()
+
+        self.fc_layer_2 = nn.Linear(256, 128) #256 and 128 features (these can be changed)
+        self.activation_func_2 = nn.ReLU()
+
+        self.fc_layer_3 = nn.Linear(128, 10) #there are 10 classes
+
+    def forward(self, x):
+        '''Forward propagation'''
+        h1 = self.activation_func_1(self.fc_layer_1(x))
+        h2 = self.activation_func_2(self.fc_layer_2(h1))
+        y_pred = self.fc_layer_3(h2)   #outputs raw scores (logits), not probabilities
+        return y_pred
+
+    def train(self, X, y):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate) #Adam = optimization algorithm (updates weights)
+        loss_function = nn.CrossEntropyLoss()
+        accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
+
+        X_tensor = torch.FloatTensor(np.array(X))
+        y_tensor = torch.LongTensor(np.array(y))
+
+        for epoch in range(self.max_epoch):
+            y_pred = self.forward(X_tensor)
+            train_loss = loss_function(y_pred, y_tensor)
+
+            #backpropogation
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+            if epoch % 1 == 0:
+                accuracy_evaluator.data = {
+                    'true_y': y_tensor,
+                    'pred_y': y_pred.max(1)[1]
+                }
+                print('Epoch:', epoch,
+                      'Accuracy:', accuracy_evaluator.evaluate(),
+                      'Loss:', train_loss.item())
+
+    def test(self, X):
+        self.eval()  # switch to evaluation mode
+        X_tensor = torch.FloatTensor(np.array(X))
+    
+        with torch.no_grad():  #no gradients needed as during testing, we don’t update weights
+            y_pred = self.forward(X_tensor)
+
+        return y_pred.max(1)[1]
+
+    def run(self):
+        print('method running...')
+        print('--start training...')
+        self.train(self.data['train']['X'], self.data['train']['y'])
+        print('--start testing...')
+        pred_y = self.test(self.data['test']['X'])
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
